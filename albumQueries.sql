be5cd28b2916d421ddd781db24fe723593be3d71
sroscoe2 <51931272+sroscoe2@users.noreply.github.com>
2021-06-25 16:20:16 -0500

    update for summer 2021

--- albumQueries.sql
+++ albumQueries.sql
@@ -12,6 +12,11 @@
 -- verify your work. If you do, write your queries in the script file
 -- provided rather than hand-writing your queries here.
 
+-- ----------------------
+-- 3.1 Executing Queries
+-- ----------------------
+
+
 -- Simple Queries 
 -- --------------
 
@@ -65,5 +70,44 @@
 
 -- 16. Write a query to find all musicians that are in more than one band.
 
+-- ----------------------
+-- Part 3.2
+-- ----------------------
 
+
+-- 1. Choose your favorite album and insert it into the database by doing the
+--   following.
+--   1.  Write a query to insert the band of the album 
+
+--   2.  Write a query to insert the album 
+
+--   3.  Write two queries to insert the first two songs of the album
+
+--   4.  Write two queries to associate the two songs with the inserted album
+
+-- 2. Update the musician record for "P. Best", his first name should be "Pete".
+
+-- 3. Pete Best was the Beatles original drummer, but was fired in 1962. 
+--    Write a query that removes Pete Best from the Beatles.
+
+-- 4. Attempt to delete the song "Big in Japan" (by Tom Waits on the album
+--    *Mule Variations*).  Observe that the query will fail due to referencing
+--    records. Write a series of queries that will allow you to delete the 
+--    album *Mule Variations*.
+
+-- Part 3.2.2
+-- Write quries to create your new tables for concerts, venues, etc. here:
+
+
+
+-- Part 3.3.3
+-- 
+-- Insert some test data to your new tables
+--
+-- 1.  Write queries to insert at least two `Concert` records.
+
+-- 2.  Write queries to associate at least 2 songs with each of the two concerts
+
+-- 3.  Write a select-join query to retrieve these new results and produce
+--     a playlist for each concert
 
